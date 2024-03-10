1e1b1a7a5d66a996dd48f343f54e54d363615e1c
Mon Albert Gamil <mrgamilmonalbert@gmail.com>
2024-03-11 04:25:36 +0800

    update home page

--- src/views/home/IndexView.vue
+++ src/views/home/IndexView.vue
@@ -1,29 +1,58 @@
 <template>
-  <section class="container h-full grid place-items-center">
-    <!-- <p class="border-neutral-300 bg-neutral-100 text-neutral-700 p-4 border rounded-lg row-span-2">
-      A student researchers of Bachelor of Science in Electronics Engineering at University of Rizal
-      System - Morong created a study that adapts the new technology which benefits the aquaculture
-      sector.
-    </p>
+  <section class="container pt-10">
+    <div
+      class="border-neutral-300 bg-neutral-100 text-neutral-700 p-4 border rounded-lg row-span-2 grid gap-16 relative"
+    >
+      <span
+        class="bg-primary text-primary-foreground px-4 py-2 font-bold text-xs uppercase rounded-t-lg absolute left-4 -top-8"
+      >
+        Overview
+      </span>
 
-    <p class="border-neutral-300 bg-neutral-100 text-neutral-700 p-4 border rounded-lg">
-      Linear regression is a foundational machine learning model used in various fields, including
-      fisheries and aquaculture. Through a smart monitoring system for Ayungin fish growth, linear
-      regression helps predict the growth of Ayungin fish based on the following water quality
-      parameters data.
-    </p>
+      <div>
+        <img
+          src="@/assets/images/UrsmFacade.webp"
+          class="w-full h-96 object-cover object-center rounded-lg"
+        />
 
-    <p class="border-neutral-300 bg-neutral-100 text-neutral-700 p-4 border rounded-lg">
-      This model focuses on analyzing the relationship between independent variables like pH level
-      and temperature level and the dependent variable, which is the weight of Ayungin fish. By
-      monitoring and collecting datasets on the following water quality parameters, the smart
-      monitoring system can build a linear regression model to predict how changes in the
-      independent variables affect the growth of Ayungin fish.
-    </p> -->
+        <p class="pt-4 text-center font-bold">
+          A student researchers of Bachelor of Science in Electronics Engineering at University of
+          Rizal System - Morong <br />
+          created a study that adapts the new technology which benefits the aquaculture sector.
+        </p>
+      </div>
 
-    <p class="border-yellow-300 bg-yellow-100 text-yellow-600 px-4 py-2 border rounded-lg">
-      Please refrain from accessing the website
-    </p>
+      <div class="relative">
+        <img
+          src="https://www.ibm.com/content/dam/connectedassets-adobe-cms/worldwide-content/creative-assets/s-migr/ul/g/04/91/watson-banking-leadspace-medium.component.xl.ts=1708973495767.png/content/adobe-cms/us/en/topics/linear-regression/jcr:content/root/leadspace"
+          class="w-full"
+        />
+
+        <div class="w-1/2 space-y-4 absolute top-[calc(520.46px/2.5)] left-4">
+          <p class="text-4xl font-semibold uppercase">Linear Regression</p>
+          <p>
+            Linear regression is a foundational machine learning model used in various fields,
+            including fisheries and aquaculture. Through a smart monitoring system for Ayungin fish
+            growth, linear regression helps predict the growth of Ayungin fish based on the
+            following water quality parameters data.
+          </p>
+        </div>
+      </div>
+
+      <div class="flex items-center gap-4">
+        <img
+          src="https://www.kaggleusercontent.com/kf/48636098/eyJhbGciOiJkaXIiLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0..fOC7Mp08l-YU1_xMNpbCjQ.hUQ5MJYMFU1qPqRGx9wfpo570NA8JreSsPWajKx7Jz5wZUhPs105Z17OMf9-VrKGwX0efU-4Sc4kAo9jHQtYBG4n55lBqo71FGhDvlocPSJBQO44BoWIXdcmph03HZfURaBwk68FPD8Lwtr2De9m2xlBM9S68_rWfrNnWZItpYaDBWbJ-ZoM4xghpy138YdkcbX4VdrHbQ0VTEPOaKTnb9UBPWQ1CyIWKaFLh0iqo1PoCxXacknQGAjqYa5vGdLtH66Uwb_5fuRdQ5u8kgHg3Lj-bhL6PWK3GF4ua7NjNK8601faJ3VEEkyv2u-NVExR0z7Yky4xZs-8vurFEFagA68Xw5L-fGGIPb5O5M_tiVJqWVX4lDGW3Y6OH4QkkkEvI17hZGiQ-CYaqe6-J0ueYrVBT-GolC22hOHOqg92PxpLc6BNOIDXDWkz7KJcMG2pQG-c4_5r1Cmqrwju6fHR_lanKB-9-MiXzG6xJ9LJqW4el-U5msP4z1YR_XL6k4EUdp1ej1kR3nGgf-CMVMEoHg5MVyB4q4_Rzma43y2ECC7f3xAebwNBFMiCLB5qxhe3uTiVtgyy0BpOtaJCjL89UBRMw0XkDECFlJJuuuWwXq8uD7C6Y7a0c6uHKC1XyyMRQNQYt4xE7MLIuv5l9c1KYa9QrOHp3xBg6_MjxItzp9wfes9r8ijfM4r1Nmk4mFZWQXZhd2c6n8hniLwrOz0T5A.PENSsdZW_tE0pfXHfdMQsw/__results___files/__results___55_1.png"
+        />
+
+        <p class="col-span-2 indent-4">
+          This model focuses on analyzing the relationship between independent variables like pH
+          level and temperature level and the dependent variable, which is the weight of Ayungin
+          fish. By monitoring and collecting datasets on the following water quality parameters, the
+          smart monitoring system can build a linear regression model to predict how changes in the
+          independent variables affect the growth of Ayungin fish.
+        </p>
+      </div>
+    </div>
   </section>
 </template>
 
