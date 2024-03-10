ba17ab8639026b6f2aed6b8a11c01256c72f0af1
Mon Albert Gamil <mrgamilmonalbert@gmail.com>
2024-03-11 05:29:07 +0800

    update parameters

--- src/views/parameters/IndexView.vue
+++ src/views/parameters/IndexView.vue
@@ -1,49 +1,49 @@
 <template>
   <section class="container pt-10">
     <div
-      class="border-neutral-300 bg-neutral-100 text-neutral-700 p-4 border rounded-lg row-span-2 grid gap-4 relative"
+      class="border-neutral-300 bg-neutral-100 text-neutral-700 h-[calc(100vh-9.35rem)] p-4 border rounded-lg row-span-2 flex flex-col gap-4 relative"
     >
       <span
-        class="bg-primary text-primary-foreground px-4 py-2 font-bold text-xs uppercase rounded-t-lg absolute left-4 -top-8"
+        class="bg-primary text-primary-foreground px-4 py-2 font-bold text-xs uppercase rounded-t-lg absolute left-4 -top-8 z-50"
       >
         Parameter values
       </span>
 
       <Button @click="handleFeedingToggling" class="w-max ml-auto">Toggle feeding time</Button>
 
-      <div class="grid grid-cols-4 gap-2">
+      <div class="flex-1 grid grid-cols-2 gap-2">
         <div
-          class="border-sky-300 bg-sky-100 text-sky-600 p-4 border rounded-lg flex items-center justify-between"
+          class="border-sky-300 bg-sky-100 text-sky-600 p-4 border rounded-lg flex flex-col items-center justify-center gap-4"
         >
-          <p class="font-bold uppercase">Feeding Time</p>
-          <p class="bg-sky-600 text-primary-foreground px-4 py-2 rounded-lg">
+          <p class="text-xl font-bold uppercase">Feeding Time</p>
+          <p class="bg-sky-600 text-primary-foreground px-4 py-2 text-4xl rounded-lg">
             {{ sensorValues.FeedingTime }}
           </p>
         </div>
 
         <div
-          class="border-amber-300 bg-amber-100 text-amber-600 p-4 border rounded-lg flex items-center justify-between"
+          class="border-amber-300 bg-amber-100 text-amber-600 p-4 border rounded-lg flex flex-col items-center justify-center gap-4"
         >
-          <p class="font-bold uppercase">Temperature</p>
-          <p class="bg-amber-600 text-primary-foreground px-4 py-2 rounded-lg">
+          <p class="text-xl font-bold uppercase">Temperature</p>
+          <p class="bg-amber-600 text-primary-foreground px-4 py-2 text-4xl rounded-lg">
             {{ sensorValues.TemperatureValue }}
           </p>
         </div>
 
         <div
-          class="border-fuchsia-300 bg-fuchsia-100 text-fuchsia-600 p-4 border rounded-lg flex items-center justify-between"
+          class="border-fuchsia-300 bg-fuchsia-100 text-fuchsia-600 p-4 border rounded-lg flex flex-col items-center justify-center gap-4"
         >
-          <p class="font-bold uppercase">Turbidity</p>
-          <p class="bg-fuchsia-600 text-primary-foreground px-4 py-2 rounded-lg">
+          <p class="text-xl font-bold uppercase">Turbidity</p>
+          <p class="bg-fuchsia-600 text-primary-foreground px-4 py-2 text-4xl rounded-lg">
             {{ sensorValues.TurbidityValue }}
           </p>
         </div>
 
         <div
-          class="border-emerald-300 bg-emerald-100 text-sky-600 p-4 border rounded-lg flex items-center justify-between"
+          class="border-emerald-300 bg-emerald-100 text-sky-600 p-4 border rounded-lg flex flex-col items-center justify-center gap-4"
         >
-          <p class="font-bold uppercase">PH</p>
-          <p class="bg-emerald-600 text-primary-foreground px-4 py-2 rounded-lg">
+          <p class="text-xl font-bold uppercase">PH</p>
+          <p class="bg-emerald-600 text-primary-foreground px-4 py-2 text-4xl rounded-lg">
             {{ sensorValues.pHValue }}
           </p>
         </div>
