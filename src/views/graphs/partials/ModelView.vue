5db1c3d2f24e92fafd05a1723d7d61eaad2eae96
Mon Albert Gamil <67058406+mnlbrtgml@users.noreply.github.com>
2024-03-15 03:14:18 +0800

    add changes to types

--- src/views/graphs/partials/ModelView.vue
+++ src/views/graphs/partials/ModelView.vue
@@ -2,13 +2,16 @@
   <div class="flex flex-col gap-4">
     <div class="flex items-center justify-between bg-gray-100 p-4 rounded-lg shadow-md">
       <p class="font-semibold text-lg">
-        Latest predicted weight: {{ predictions[predictions.length - 1]?.predicted_weight }} kg
+        Latest predicted weight: {{ predictions && predictions[predictions.length - 1]?.predicted_weight }} kg
       </p>
-      
-      <Button @click="getPredictions" class="w-max bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Refresh</Button>
+
+      <Button
+        @click="getPredictions"
+        class="w-max bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >Refresh</Button
+      >
     </div>
     <div v-if="predictions && predictions.length > 0" class="flex-1">
-     
       <div class="grid grid-cols-2 gap-4 mt-4">
         <!-- Predicted Weight Chart -->
         <div class="grid place-items-center">
@@ -26,9 +29,8 @@
         <div class="grid place-items-center">
           <Bar :options="chartOptions" :data="phChartData" />
         </div>
-      
       </div>
-       <div class="overflow-auto max-h-[29rem]">
+      <div class="overflow-auto max-h-[29rem]">
         <table class="w-full text-left shadow rounded-lg">
           <thead class="bg-gray-200">
             <tr>
@@ -40,7 +42,11 @@
             </tr>
           </thead>
           <tbody>
-            <tr v-for="(prediction, index) in predictions" :key="index" :class="index % 2 === 0 ? 'bg-gray-50' : 'bg-white'">
+            <tr
+              v-for="(prediction, index) in predictions"
+              :key="index"
+              :class="index % 2 === 0 ? 'bg-gray-50' : 'bg-white'"
+            >
               <td class="px-4 py-2 border-b">{{ prediction.timestamp }}</td>
               <td class="px-4 py-2 border-b">{{ prediction.TemperatureValue }}</td>
               <td class="px-4 py-2 border-b">{{ prediction.TurbidityValue }}</td>
@@ -67,12 +73,13 @@ import {
   BarElement,
   CategoryScale,
   LinearScale,
+  type ChartData
 } from "chart.js";
 import Button from "@/components/ui/button/Button.vue";
 import { format } from "date-fns";
 
 interface Prediction {
-  timestamp: firebase.firestore.Timestamp;
+  timestamp: string;
   TemperatureValue: number;
   TurbidityValue: number;
   pHValue: number;
@@ -81,15 +88,15 @@ interface Prediction {
 
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
 
-const predictions = ref([]);
-const chartOptions = reactive({
+const predictions = ref<Prediction[]>();
+const chartOptions = reactive<any>({
   responsive: true,
   plugins: {
     legend: {
-      position: 'top',
+      position: "top",
     },
     tooltip: {
-      mode: 'index',
+      mode: "index",
       intersect: false,
     },
   },
@@ -101,25 +108,31 @@ const chartOptions = reactive({
     },
     y: {
       grid: {
-        color: 'rgba(240, 240, 240, 1)',
+        color: "rgba(240, 240, 240, 1)",
       },
     },
   },
 });
 
-const temperatureChartData = reactive(createChartData('Temperature (°C)', 'rgba(255, 99, 132, 0.5)'));
-const turbidityChartData = reactive(createChartData('Turbidity (NTU)', 'rgba(210, 162, 235, 0.5)'));
-const phChartData = reactive(createChartData('pH Level', 'rgba(255, 206, 86, 0.5)'));
-const predictedWeightChartData = reactive(createChartData('Predicted Weight (g)', 'rgba(75, 192, 192, 0.5)'));
+const temperatureChartData = reactive(
+  createChartData("Temperature (°C)", "rgba(255, 99, 132, 0.5)"),
+);
+const turbidityChartData = reactive(createChartData("Turbidity (NTU)", "rgba(210, 162, 235, 0.5)"));
+const phChartData = reactive(createChartData("pH Level", "rgba(255, 206, 86, 0.5)"));
+const predictedWeightChartData = reactive(
+  createChartData("Predicted Weight (g)", "rgba(75, 192, 192, 0.5)"),
+);
 
 function createChartData(label: string, backgroundColor: string) {
   return {
     labels: [],
-    datasets: [{
-      label,
-      data: [],
-      backgroundColor: backgroundColor,
-    }],
+    datasets: [
+      {
+        label,
+        data: [],
+        backgroundColor: backgroundColor,
+      },
+    ],
   };
 }
 
@@ -128,22 +141,39 @@ async function getPredictions(): Promise<void> {
   const predictionCollection = await getDocs(predictionsQuery);
 
   if (predictionCollection) {
-    const tempList: Prediction[] =[];
-    let timestamps: firebase.firestore.Timestamp[] = [];
+    const tempList: Prediction[] = [];
+    let timestamps: string[] = [];
 
     predictionCollection.forEach((doc) => {
       const data = doc.data() as Prediction;
       tempList.push(data);
-      timestamps.push(data.timestamp);
+      timestamps.push(data.timestamp.toString());
     });
 
     predictions.value = tempList;
 
-  timestamps = predictions.value.map(p => format(new Date(p.timestamp), 'HH:mm'));
-    updateChartData(temperatureChartData, timestamps, tempList.map(t => t.TemperatureValue));
-    updateChartData(turbidityChartData, timestamps, tempList.map(t => t.TurbidityValue));
-    updateChartData(phChartData, timestamps, tempList.map(t => t.pHValue));
-    updateChartData(predictedWeightChartData, timestamps, tempList.map(t => t.predicted_weight));
+    timestamps = predictions.value.map((p) => format(new Date(p.timestamp.toString()), "HH:mm"));
+    
+    updateChartData(
+      temperatureChartData,
+      timestamps,
+      tempList.map((t) => t.TemperatureValue),
+    );
+    updateChartData(
+      turbidityChartData,
+      timestamps,
+      tempList.map((t) => t.TurbidityValue),
+    );
+    updateChartData(
+      phChartData,
+      timestamps,
+      tempList.map((t) => t.pHValue),
+    );
+    updateChartData(
+      predictedWeightChartData,
+      timestamps,
+      tempList.map((t) => t.predicted_weight),
+    );
   }
 }
 
@@ -154,7 +184,3 @@ function updateChartData(chartData: ChartData, labels: string[], data: number[])
 
 await getPredictions();
 </script>
-
-<style scoped>
-/* Add additional styling here */
-</style>
